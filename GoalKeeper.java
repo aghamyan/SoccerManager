f5c1ab1645e4d4dda364f0cad8512f9313000554
aghamyan <57287656+aghamyan@users.noreply.github.com>
2023-05-02 00:36:11 +0400

    Update GoalKeeper.java

--- GoalKeeper.java
+++ GoalKeeper.java
@@ -1,17 +1,38 @@
-class GoalKeeper {
-    private String name;
-    private int rating;
+public class GoalKeeper extends Player {
+    private int diving;
+    private int handling;
+    private int reflexes;
 
-    public GoalKeeper(String name, int rating) {
-        this.name = name;
-        this.rating = rating;
+    public GoalKeeper(String name, int pac, int shot, int dribbling, int pass, int phys, int def, int age,
+                      int diving, int handling, int reflexes) {
+        super(name, pac, shot, dribbling, pass, phys, def, age);
+        this.diving = diving;
+        this.handling = handling;
+        this.reflexes = reflexes;
     }
 
-    public String getName() {
-        return name;
+    // Getters and setters
+    public int getDiving() {
+        return diving;
     }
 
-    public int getRating() {
-        return rating;
+    public void setDiving(int diving) {
+        this.diving = diving;
+    }
+
+    public int getHandling() {
+        return handling;
+    }
+
+    public void setHandling(int handling) {
+        this.handling = handling;
+    }
+
+    public int getReflexes() {
+        return reflexes;
+    }
+
+    public void setReflexes(int reflexes) {
+        this.reflexes = reflexes;
     }
 }
