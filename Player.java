8771e12cea2a06a63ea7c5328a2b7018080493b7
aghamyan <57287656+aghamyan@users.noreply.github.com>
2023-05-02 00:36:50 +0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,32 +1,89 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Player {
-    private int attack;
-    private int save;
+    private String name;
+    private int age;
+    private int pac;
+    private int shot;
+    private int dribbling;
+    private int pass;
+    private int phys;
+    private int def;
 
-    public Player(int attack, int save) {
-        this.attack = attack;
-        this.save = save;
+
+    public String getName() {
+        return name;
     }
 
-    public int getAttack() {
-        return attack;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setAttack(int attack) {
-        this.attack = attack;
+    public Player(String name, int pac, int shot, int dribbling, int pass, int phys, int def, int age) {
+        this.name = name;
+        this.pac = pac;
+        this.shot = shot;
+        this.dribbling = dribbling;
+        this.pass = pass;
+        this.phys = phys;
+        this.def = def;
+        this.age = age;
     }
 
-    public int getSave() {
-        return save;
+    // Getters and setters
+    public int getPac() {
+        return pac;
     }
 
-    public void setSave(int save) {
-        this.save = save;
+    public void setPac(int pac) {
+        this.pac = pac;
     }
 
-    public boolean isGoal(Player opponent) {
-        return this.attack > opponent.getSave();
+    public int getShot() {
+        return shot;
     }
+
+    public void setShot(int shot) {
+        this.shot = shot;
+    }
+
+    public int getDribbling() {
+        return dribbling;
+    }
+
+    public void setDribbling(int dribbling) {
+        this.dribbling = dribbling;
+    }
+
+    public int getPass() {
+        return pass;
+    }
+
+    public void setPass(int pass) {
+        this.pass = pass;
+    }
+
+    public int getPhys() {
+        return phys;
+    }
+
+    public void setPhys(int phys) {
+        this.phys = phys;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
